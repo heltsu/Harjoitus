560f5397a7722f4b5620fffa7ec773ba9ec9a934
Heli Niemimaa <hniemima@melkki.cs.helsinki.fi>
2012-09-12 11:05:57 +0300

    kokeilu

---
+++ Kortisto/src/Ruokakortisto.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Ruokakortisto {
+     String nimi;
+    int vaikeusaste;
+    String kategoria;
+    ArrayList<String> reseptit = new ArrayList<String>();
+
+    public Ruokakortisto(String nimi, int vaikeusaste) {
+        this.nimi = nimi;
+        this.vaikeusaste = vaikeusaste;
+    }
+
+    public String haeNimi() {
+
+        return nimi;
+    }
+
+    public String haeKategoria() {
+
+        return kategoria;
+    }
+
+    public int haeVaikeusaste() {
+        if (vaikeusaste <= 0) {
+            return 0;
+        } else if (vaikeusaste >= 5) {
+            return 5;
+        } else {
+            return vaikeusaste;
+        }
+    }
+
+    public void lisaaRespeti(String nimi) {
+
+        reseptit.add(nimi);
+    }
+}
